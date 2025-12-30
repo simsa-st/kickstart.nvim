0f5a11d7054dfa0f6e4409695530118594e4bfc2
Stepan Simsa <simsa.st@gmail.com>
2025-12-30 12:31:53 +0100

    format: autoformat using ruff

--- lua/custom/plugins/format.lua
+++ lua/custom/plugins/format.lua
@@ -32,6 +32,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
+        python = { 'ruff' },
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
